dd80a4c77c82b385d2ef8901aa734135315f2be8
danieljunek15 <danieljunek93@gmail.com>
2022-06-30 16:43:20 +0200

    auto scale

--- resources/views/diensten.blade.php
+++ resources/views/diensten.blade.php
@@ -30,64 +30,64 @@
             <div class="bg-purple-600 m-6 drop-shadow-2xl rounded-2xl">
                 <b><p class="text-2xl font-mono text-white m-6">APK</p></b>
                 <div class="m-6 text-l text-white">
-                    <b class="whitespace-pre"><h3 class="text-xl">APK Basic</h3></b>
-                    <b class="whitespace-pre">    Wat hoort er bij een basic APK ?</b><br>
-                    <b class="whitespace-pre">    Wij zorgen er voor dat de software van u laptop/desktop helemaal uptodate is.</b><br>
-                    <b class="whitespace-pre">    En zorgen we ervoor dat er geen virussen op staan, ook als u niet wist dat er een opstond.</b><br>
-                    <b class="whitespace-pre">    Ook kunnen we overige rommel van u pc weg halen.</b><br><br>
-                    <b class="whitespace-pre"><h3 class="text-xl">APK Pro</h3></b>
-                    <b class="whitespace-pre">    Is een basic APK plus de laptop/desktop word stof vrij en schoon gemaakt.</b><br>
-                    <b class="whitespace-pre">    met uitleg wat we doen en hebben gedaan.</b><br><br>
-                    <b class="whitespace-pre"><h3 class="text-xl">APK Deluxe</h3></b>
-                    <b class="whitespace-pre">    Wat hoort er bij een Deluxe APK ?</b><br>
-                    <b class="whitespace-pre">    De basic en pro APK worden uitgevoerd, en de thermal paste/pads worden vervangen van u desktop/laptop.</b><br>
-                    <b class="whitespace-pre">    Installeren we wat handige tools voor het zelfstandig kunnen updaten in de toekomst.</b><br>
-                    <b class="whitespace-pre">    Met uitleg over deze tools.</b><br><br>
-                    <b class="whitespace-pre">Wij gebruiken <a class="text-xl" href="/tarieven">dit</a> als bron voor de kosten.</b>                   
+                    <b><h3 class="text-xl">APK Basic</h3></b>
+                    <b>    Wat hoort er bij een basic APK ?</b><br>
+                    <b>    Wij zorgen er voor dat de software van u laptop/desktop helemaal uptodate is.</b><br>
+                    <b>    En zorgen we ervoor dat er geen virussen op staan, ook als u niet wist dat er een opstond.</b><br>
+                    <b>    Ook kunnen we overige rommel van u pc weg halen.</b><br><br>
+                    <b><h3 class="text-xl">APK Pro</h3></b>
+                    <b>    Is een basic APK plus de laptop/desktop word stof vrij en schoon gemaakt.</b><br>
+                    <b>    met uitleg wat we doen en hebben gedaan.</b><br><br>
+                    <b><h3 class="text-xl">APK Deluxe</h3></b>
+                    <b>    Wat hoort er bij een Deluxe APK ?</b><br>
+                    <b>    De basic en pro APK worden uitgevoerd, en de thermal paste/pads worden vervangen van u desktop/laptop.</b><br>
+                    <b>    Installeren we wat handige tools voor het zelfstandig kunnen updaten in de toekomst.</b><br>
+                    <b>    Met uitleg over deze tools.</b><br><br>
+                    <b>Wij gebruiken <a class="text-xl" href="/tarieven">dit</a> als bron voor de kosten.</b>                   
                 </div>
             </div>
             <div class="bg-white m-6 shadow-xl rounded-2xl">
                 <b><p class="text-2xl font-mono m-6">Hulp bij het bouwen van u desktop</p></b>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre">-   Wij kunnen assisteren bij het bouw procces.</b><br>
-                    <b class="whitespace-pre">-   Wij kunnen het procces compleet over nemen, en alles nettjes gebruiks klaar maken.</b><br>
-                    <b class="whitespace-pre">-   Wij kunnen helpen met het modificeren/upgraden van u desktop/laptop.</b><br>
-                    <b class="whitespace-pre">-   En als extra kunnen wij u zelfs les geven in het bouwen/installeren van een desktop/laptop.</b><br><br>
-                    <b class="whitespace-pre">Wij hanteren een <a class="text-xl" href="/tarieven">uur tarief</a> voor de kosten berekening. (thermal paste voor CPU inbegrepen)</b>
+                    <b>-   Wij kunnen assisteren bij het bouw procces.</b><br>
+                    <b>-   Wij kunnen het procces compleet over nemen, en alles nettjes gebruiks klaar maken.</b><br>
+                    <b>-   Wij kunnen helpen met het modificeren/upgraden van u desktop/laptop.</b><br>
+                    <b>-   En als extra kunnen wij u zelfs les geven in het bouwen/installeren van een desktop/laptop.</b><br><br>
+                    <b>Wij hanteren een <a class="text-xl" href="/tarieven">uur tarief</a> voor de kosten berekening. (thermal paste voor CPU inbegrepen)</b>
                 </div>  
             </div>
             
             <div class="bg-white m-6 shadow-xl rounded-2xl">
                 <b><p class="text-2xl font-mono m-6">Advies geven en diagnose stellen</p></b>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre">Wat kan Fitserz voor u hier in betekenen ?</b><br>
-                    <b class="whitespace-pre">-   Als u zelf niet weet wat er aan de hand is met u aparaat,</b><br> 
-                    <b class="whitespace-pre">    kunt u Flitserz inhuren voor een diagnose op het angegeven probleem.</b><br>
-                    <b class="whitespace-pre">-   Het informeren wat er momenteel aan de hand is, en de oplossing(en) voor u op tafel leggen.</b><br>
-                    <b class="whitespace-pre">-   Wij maken dan een offerte met de handelingen en de kosten die hierbij komen kijken.</b><br><br>
-                    <b class="whitespace-pre">Wij gebruiken <a class="text-xl" href="/tarieven">dit</a> als bron voor de kosten.</b>
+                    <b>Wat kan Fitserz voor u hier in betekenen ?</b><br>
+                    <b>-   Als u zelf niet weet wat er aan de hand is met u aparaat,</b><br> 
+                    <b>    kunt u Flitserz inhuren voor een diagnose op het angegeven probleem.</b><br>
+                    <b>-   Het informeren wat er momenteel aan de hand is, en de oplossing(en) voor u op tafel leggen.</b><br>
+                    <b>-   Wij maken dan een offerte met de handelingen en de kosten die hierbij komen kijken.</b><br><br>
+                    <b>Wij gebruiken <a class="text-xl" href="/tarieven">dit</a> als bron voor de kosten.</b>
                 </div>
             </div>
 
             <div class="bg-white m-6 shadow-xl rounded-2xl">
                 <b><p class="text-2xl font-mono m-6">Hulp bij het uitzoeken, installeren en instellen van elektronica.</p></b>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre"><h2 class="text-xl">Wat kan Fitserz voor u hier in betekenen ?</h2></b>
-                    <b class="whitespace-pre">-   Het helpen uitzoeken van een gepast apparaat gebazeerd op u prijs en wensen.</b><br>
-                    <b class="whitespace-pre">-   Een eerlijke mening geven over de gekozen apparaten, dus de voordelen en nadelen benadruken.</b><br>
-                    <b class="whitespace-pre">-   De beste prijs voor het gekozen aparaat vinden.</b><br>
-                    <b class="whitespace-pre">-   Wanneer u het apparaat (al) binnen hebt, installeren wij het voledig voor u.</b><br>
-                    <b class="whitespace-pre">-   Wij kunnen aanpassingen maken, bijv instellingen aan passen.</b><br><br>
-                    <b class="whitespace-pre"><h2 class="text-xl">Wat kunt u verwachten van Flitserz ?</h2></b>
-                    <b class="whitespace-pre">    Als eerste willen wij meegeven dat Flitserz niks verdiend aan het product dat wij speciaal voor u aanraden/uitzoeken.</b><br>
-                    <b class="whitespace-pre">    Wij zoeken enkel naar de beste opties voor u, gebazeerd op u verzoek(en). Bijv prijs, functionaliteit en kwaliteit.</b><br>
-                    <b class="whitespace-pre">    De keuzen van het product ligt dus bij u, wij doen enkel onderzoek plegen van uit een profesioneel oog.</b><br>
-                    <b class="whitespace-pre">    Dus wij consulten alleen over de mogelijkheden met u.</b><br>
-                    <b class="whitespace-pre">    Daarom vragen wij alleen het uur tarief voor het adviseren, informeren, bijleren, installeren en instellen.</b><br>
-                    <b class="whitespace-pre">    De gratis diagnose geld ook voor adviseren.</b><br>
-                    <b class="whitespace-pre">    Wij luisteren dus eerst naar u verhaal, beschrijving en voorkeuren.</b><br>
-                    <b class="whitespace-pre">    Wanneer we u begrijpen gaan we de tijd opnemen, en zo snel mogelijk zoeken terwijl u mee kijkt. (en uitleg krijgt)</b><br><br>
-                    <b class="whitespace-pre">Wij gebruiken <a class="text-xl" href="/tarieven">dit</a> als bron voor de kosten.</b>
+                    <b><h2 class="text-xl">Wat kan Fitserz voor u hier in betekenen ?</h2></b>
+                    <b>-   Het helpen uitzoeken van een gepast apparaat gebazeerd op u prijs en wensen.</b><br>
+                    <b>-   Een eerlijke mening geven over de gekozen apparaten, dus de voordelen en nadelen benadruken.</b><br>
+                    <b>-   De beste prijs voor het gekozen aparaat vinden.</b><br>
+                    <b>-   Wanneer u het apparaat (al) binnen hebt, installeren wij het voledig voor u.</b><br>
+                    <b>-   Wij kunnen aanpassingen maken, bijv instellingen aan passen.</b><br><br>
+                    <b><h2 class="text-xl">Wat kunt u verwachten van Flitserz ?</h2></b>
+                    <b>    Als eerste willen wij meegeven dat Flitserz niks verdiend aan het product dat wij speciaal voor u aanraden/uitzoeken.</b><br>
+                    <b>    Wij zoeken enkel naar de beste opties voor u, gebazeerd op u verzoek(en). Bijv prijs, functionaliteit en kwaliteit.</b><br>
+                    <b>    De keuzen van het product ligt dus bij u, wij doen enkel onderzoek plegen van uit een profesioneel oog.</b><br>
+                    <b>    Dus wij consulten alleen over de mogelijkheden met u.</b><br>
+                    <b>    Daarom vragen wij alleen het uur tarief voor het adviseren, informeren, bijleren, installeren en instellen.</b><br>
+                    <b>    De gratis diagnose geld ook voor adviseren.</b><br>
+                    <b>    Wij luisteren dus eerst naar u verhaal, beschrijving en voorkeuren.</b><br>
+                    <b>    Wanneer we u begrijpen gaan we de tijd opnemen, en zo snel mogelijk zoeken terwijl u mee kijkt. (en uitleg krijgt)</b><br><br>
+                    <b>Wij gebruiken <a class="text-xl" href="/tarieven">dit</a> als bron voor de kosten.</b>
                 </div>
             </div>
         </div>
--- resources/views/over.blade.php
+++ resources/views/over.blade.php
@@ -30,39 +30,39 @@
             <div class="bg-purple-600 m-6 drop-shadow-2xl rounded-2xl">
                 <b><p class="text-2xl font-mono text-white m-6">Over ons Flitserz</p></b>
                 <div class="m-6 text-l text-white">
-                    <b class="whitespace-pre"><h3 class="text-xl">Wie zijn wij</h3></b>
-                    <b class="whitespace-pre">    Flitserz is een jong huis aan huis ICT oplossing voor jong en oud.</b><br>
-                    <b class="whitespace-pre">    Als u niet uit huis kan, te oud bent om u computer naar een winkel te brengen.</b><br>
-                    <b class="whitespace-pre">    Of u heeft betere dingen te doen en wilt liever iemand langs laten komen.</b><br>
-                    <b class="whitespace-pre">    Dan zijn wij de oplossing voor u.</b><br>
-                    <b class="whitespace-pre">    Wij zijn de premium huis aan huis IT oplossing voor u.</b><br><br>
-                    <b class="whitespace-pre"><h3 class="text-xl">Wat is het doel van Flitserz</h3></b>
-                    <b class="whitespace-pre">    Flitserz streeft er naar om de beste, voordeligste en vriendelijkste IT oplossing te worden van u buurt.</b><br>
-                    <b class="whitespace-pre">    Wij zijn uit op klantvriendelijk ondernemen, en u tevredenheid van onze service.</b><br>
-                    <b class="whitespace-pre">    De wereld om ons heen een stukje slimmer en beter maken elke dag weer,</b><br>
-                    <b class="whitespace-pre">    door u de beste service te geven dat menselijkmogelijk is.</b><br>
+                    <b><h3 class="text-xl">Wie zijn wij</h3></b>
+                    <b>    Flitserz is een jong huis aan huis ICT oplossing voor jong en oud.</b><br>
+                    <b>    Als u niet uit huis kan, te oud bent om u computer naar een winkel te brengen.</b><br>
+                    <b>    Of u heeft betere dingen te doen en wilt liever iemand langs laten komen.</b><br>
+                    <b>    Dan zijn wij de oplossing voor u.</b><br>
+                    <b>    Wij zijn de premium huis aan huis IT oplossing voor u.</b><br><br>
+                    <b><h3 class="text-xl">Wat is het doel van Flitserz</h3></b>
+                    <b>    Flitserz streeft er naar om de beste, voordeligste en vriendelijkste IT oplossing te worden van u buurt.</b><br>
+                    <b>    Wij zijn uit op klantvriendelijk ondernemen, en u tevredenheid van onze service.</b><br>
+                    <b>    De wereld om ons heen een stukje slimmer en beter maken elke dag weer,</b><br>
+                    <b>    door u de beste service te geven dat menselijkmogelijk is.</b><br>
                 </div>
             </div>
 
             <div class="bg-white m-6 shadow-xl rounded-2xl">
                 <b><p class="text-2xl font-mono m-6">Klant vriendelijk</p></b>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre">Omdat Flitserz een jong bedrijf is willen wij zo goed mogelijk cominucatie hebben tussen u en ons.</b><br>
-                    <b class="whitespace-pre">Wij hanteren daarom een "No fix no pay" beleid.</b><br>
-                    <b class="whitespace-pre">Flitserz bied een gratis diagnose aan, voor idereen die dit graag wilt.</b><br>
-                    <b class="whitespace-pre">Na de diagnose word er aangegeven wat er mogelijk is en of het opgelost kan worden.</b><br>
+                    <b>Omdat Flitserz een jong bedrijf is willen wij zo goed mogelijk cominucatie hebben tussen u en ons.</b><br>
+                    <b>Wij hanteren daarom een "No fix no pay" beleid.</b><br>
+                    <b>Flitserz bied een gratis diagnose aan, voor idereen die dit graag wilt.</b><br>
+                    <b>Na de diagnose word er aangegeven wat er mogelijk is en of het opgelost kan worden.</b><br>
                 </div>
             </div>
 
             <div class="bg-white m-6 shadow-xl rounded-2xl">
                 <b><p class="text-2xl font-mono m-6">Ideologie van Flitserz</p></b>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre">Wij sluiten niemand uit, en willen alle techniche problemen aanpakken voor u.</b><br>
-                    <b class="whitespace-pre">No fix no pay. Als het ons niet lukt een oplossing te vinden,</b><br>
-                    <b class="whitespace-pre">dan zeggen wij dit zo snel mogelijk om u en onze tijd niet te verspillen.</b><br>
-                    <b class="whitespace-pre">Wij vragen dan ook geen betaling.</b><br>
-                    <b class="whitespace-pre">Omdat het uitleggen van een technische probleem soms erg lastig kan zijn,</b><br>
-                    <b class="whitespace-pre">kunnen we gratis langs komen om een diagnoste te stellen op u probleem.</b><br>
+                    <b>Wij sluiten niemand uit, en willen alle techniche problemen aanpakken voor u.</b><br>
+                    <b>No fix no pay. Als het ons niet lukt een oplossing te vinden,</b><br>
+                    <b>dan zeggen wij dit zo snel mogelijk om u en onze tijd niet te verspillen.</b><br>
+                    <b>Wij vragen dan ook geen betaling.</b><br>
+                    <b>Omdat het uitleggen van een technische probleem soms erg lastig kan zijn,</b><br>
+                    <b>kunnen we gratis langs komen om een diagnoste te stellen op u probleem.</b><br>
                 </div>
             </div>
 
@@ -72,28 +72,28 @@
                     <a href="https://www.linkedin.com/in/daniël-van-den-brink-4a21651a3/"><img class="drop-shadow-2xl rounded-3xl border-4 border-purple-600" src="https://media-exp1.licdn.com/dms/image/C5603AQFOpIF9YRMSew/profile-displayphoto-shrink_200_200/0/1586166595898?e=1659571200&v=beta&t=0SSk2POuM9FRvQA1BioOJt1V2MmR3YWttewc8DcG5QM" width='200' height='200'></a>
                 </div>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre"><h3 class="text-xl">Wie ben ik ?</h3></b>
-                    <b class="whitespace-pre">    Mijn naam is Daniël van den Brink, en ben 21 jaar oud.</b><br>
-                    <b class="whitespace-pre">    Momenteel ben ik naast mijn opleiding bij de Bit academy, ook de trotse eigenaar van Flitserz.</b><br>
-                    <b class="whitespace-pre">    Samen met Flitserz ben ik gelocaliseerd in Gooi en Vechtstreek.</b><br>
-                    <b class="whitespace-pre">    Momenteel ben ik de expert van het Flitserz IT team.</b><br>
-                    <b class="whitespace-pre">    Wanneer u contact met ons maakt en een afspraak wilt ben ik de gene die langs komt.</b><br>
-                    <b class="whitespace-pre">    Mijn expertise is erg breed in de IT, en sta open voor elke taak.</b><br>
-                    <b class="whitespace-pre">    Kom maar op. :D</b><br>
-                    <b class="whitespace-pre">    Als er een ding is dat ik niet kan afslaan, dan is dat een lekker kopje koffie of thee.</b><br><br>
-                    <b class="whitespace-pre"><h3 class="text-xl">Wat is mijn doel ?</h3></b>
-                    <b class="whitespace-pre">    Mijn doel is om u te helpen optimaal gebruik te kunnen maken van u apparaat.</b><br>
-                    <b class="whitespace-pre">    Een goede uitleg geven, en idereen die er voor openstaat bij te leren over het apparaat.</b><br>
-                    <b class="whitespace-pre">    Zodat ook u beter weet hoe het werkt.</b><br>
-                    <b class="whitespace-pre">    Mijn doel is dus om u te helpen en wijzer te maken. :D</b><br>
+                    <b><h3 class="text-xl">Wie ben ik ?</h3></b>
+                    <b>    Mijn naam is Daniël van den Brink, en ben 21 jaar oud.</b><br>
+                    <b>    Momenteel ben ik naast mijn opleiding bij de Bit academy, ook de trotse eigenaar van Flitserz.</b><br>
+                    <b>    Samen met Flitserz ben ik gelocaliseerd in Gooi en Vechtstreek.</b><br>
+                    <b>    Momenteel ben ik de expert van het Flitserz IT team.</b><br>
+                    <b>    Wanneer u contact met ons maakt en een afspraak wilt ben ik de gene die langs komt.</b><br>
+                    <b>    Mijn expertise is erg breed in de IT, en sta open voor elke taak.</b><br>
+                    <b>    Kom maar op. :D</b><br>
+                    <b>    Als er een ding is dat ik niet kan afslaan, dan is dat een lekker kopje koffie of thee.</b><br><br>
+                    <b><h3 class="text-xl">Wat is mijn doel ?</h3></b>
+                    <b>    Mijn doel is om u te helpen optimaal gebruik te kunnen maken van u apparaat.</b><br>
+                    <b>    Een goede uitleg geven, en idereen die er voor openstaat bij te leren over het apparaat.</b><br>
+                    <b>    Zodat ook u beter weet hoe het werkt.</b><br>
+                    <b>    Mijn doel is dus om u te helpen en wijzer te maken. :D</b><br>
                 </div>
             </div>
 
             <div class="bg-white m-6 shadow-xl rounded-2xl">
                 <b><p class="text-2xl font-mono m-6">Bedrijf info van Flitserz</p></b>
                 <div class="m-6 text-l">
-                    <b class="whitespace-pre">Naam: Flitserz</b><br>
-                    <b class="whitespace-pre">Kvk-num: 84729333</b><br>
+                    <b>Naam: Flitserz</b><br>
+                    <b>Kvk-num: 84729333</b><br>
                 </div>
             </div>
         </div>
